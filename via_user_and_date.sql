00ee8241c4935315a507836c2f7cf51463a29a3d
allfill2009 <65020891+allfill2009@users.noreply.github.com>
2020-05-08 16:33:54 +0300

    Update via_user_and_date.sql

--- via_user_and_date.sql
+++ via_user_and_date.sql
@@ -1,16 +1,23 @@
 SET @i := 0;
 SET @d := '2020-04-09 00:00:00';
-SET @user_id := 1;
-SET @database_name := '^develop$';
-SELECT CONCAT(IF ((@i := @i + 1) = 1, '', 'UNION '), 'SELECT ''SELECT * FROM `', t.TABLE_NAME, '` WHERE ', t.BY_EXPRESSION, '`', t.COLUMN_NAME, '` >= "', @d, '"; '' as "#table_name" FROM `', t.TABLE_NAME, '` WHERE ', t.BY_EXPRESSION, '`', t.COLUMN_NAME, '` >= "', @d, '"') as '#table_name' 
+SET @user_id := 1468450;
+SET @database_name := '^benefits_develop$';
+SELECT CONCAT(IF ((@i := @i + 1) = 1, '', 'UNION '),
+              'SELECT ',
+              '  ''SELECT * FROM `', t.TABLE_NAME, '`',
+              '    WHERE ', t.BY_EXPRESSION, '`', t.COLUMN_NAME, '` >= "', @d, '"; '' as "#table_name"',
+              'FROM `', t.TABLE_NAME, '`',
+              'WHERE ', t.BY_EXPRESSION, '`', t.COLUMN_NAME, '` >= "', @d, '"') as '#table_name' 
 FROM (
   SELECT c.TABLE_NAME, MAX(c.COLUMN_NAME) as COLUMN_NAME, (
     SELECT IFNULL(CONCAT('`', MAX(cc.COLUMN_NAME), '` = ', @user_id, ' AND '), '')
     FROM information_schema.`COLUMNS` cc
-	 WHERE cc.TABLE_NAME = c.TABLE_NAME AND cc.TABLE_SCHEMA = c.TABLE_SCHEMA AND cc.COLUMN_NAME IN ('created_by', 'updated_by')
+    WHERE cc.TABLE_NAME = c.TABLE_NAME AND cc.TABLE_SCHEMA = c.TABLE_SCHEMA
+      AND cc.COLUMN_NAME IN ('created_by', 'updated_by')
   ) as BY_EXPRESSION
   FROM information_schema.`COLUMNS` c
-  WHERE (@database_name IS NULL OR c.TABLE_SCHEMA RLIKE @database_name) and c.COLUMN_NAME IN ('created_at', 'updated_at')
+  WHERE (@database_name IS NULL OR c.TABLE_SCHEMA RLIKE @database_name)
+    and c.COLUMN_NAME IN ('created_at', 'updated_at')
   GROUP BY c.TABLE_NAME
   ORDER BY c.TABLE_NAME, COLUMN_NAME ASC
 ) t;
