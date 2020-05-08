f5fb595ead97c157439e474f9ccb461b0cec80f2
allfill2009 <65020891+allfill2009@users.noreply.github.com>
2020-05-08 15:17:25 +0300

    Create via_rlike.sql

---
+++ via_rlike.sql
@@ -0,0 +1,12 @@
+# RLIKE
+SET @data_type := 'char';
+SET @value := 'america';
+SET @database_name := '^develop$';
+SET @i := 0;
+SET @batch := 3000;
+SELECT CONCAT(IF((@i := @i + 1) % @batch = 1, IF(@i = 1, '', ';'), 'UNION '),
+              'SELECT ''SELECT * FROM `', TABLE_NAME, '`',
+				  '  WHERE `', COLUMN_NAME, '` RLIKE "', @value, '";'' as ''#sql''',
+				  '  FROM `', TABLE_NAME, '` WHERE `', COLUMN_NAME, '` RLIKE ''', @value, '''') AS '#tables'
+FROM information_schema.COLUMNS 
+WHERE (@database_name IS NULL OR TABLE_SCHEMA RLIKE @database_name) and DATA_TYPE rlike @data_type;
